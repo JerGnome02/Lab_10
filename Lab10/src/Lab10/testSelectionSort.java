47dee3c879870b7a3539cbb87b28c0f4106ede1f
Jerome Marudo <jerome.marudo@gmail.com>
2022-12-01 18:15:41 -0500

    testSelectionSort Changes

--- Lab10/src/Lab10/testSelectionSort.java
+++ Lab10/src/Lab10/testSelectionSort.java
@@ -6,19 +6,19 @@ import org.junit.Test;
 
 public class testSelectionSort {
 	SelectionSort temp = new SelectionSort();
-	
+
 	//@Test
 	public void test() {
-		
+
 	testPositive();
 	testNegative();
 	testMixed();
 	testDuplicates();
 	}
-	   
+
 	public testSelectionSort() {
 	}
-	
+
 	@Test
 	public void testPositive(){
 		int[] arr = new int[5];
@@ -27,24 +27,24 @@ public class testSelectionSort {
 	    arr[2] = 7;
 	    arr[3] = 10;
 	    arr[4] = 2;
-	        
+
 	    int[] Sortedarr = new int[5];
 	    Sortedarr[0] = 2;
 	    Sortedarr[1] = 7;
 	    Sortedarr[2] = 8;
 	    Sortedarr[3] = 9;
 	    Sortedarr[4] = 10;
-	    
-	    
-	        
+
+
+
 	        /** add tests to check for this unit test **/
 	    temp.basicSelectionSort(arr);
 	    for(int i = 0; i < Sortedarr.length; ++i) {
 	    	assertEquals(arr[i], Sortedarr[i]);
 	    }
-	    
+
 	    }
-	    
+
 	@Test
 	    public void testNegative(){
 			int[] arr = new int[5];
@@ -53,30 +53,30 @@ public class testSelectionSort {
 		    arr[2] = -7;
 		    arr[3] = -10;
 		    arr[4] = -2;
-		        
+
 		    int[] Sortedarr = new int[5];
 		    Sortedarr[0] = -10;
 		    Sortedarr[1] = -9;
 		    Sortedarr[2] = -8;
 		    Sortedarr[3] = -7;
 		    Sortedarr[4] = -2;
-	        
+
 	        /** Test data contains negative values only **/
 		    temp.basicSelectionSort(arr);
 		    for(int i = 0; i < Sortedarr.length; ++i) {
 		    	assertEquals(arr[i], Sortedarr[i]);
 		    }
 	    }
-	    
+
 	@Test
 	    public void testMixed(){
 			int[] arr = new int[5];
-		    arr[0] = 8;
+		    arr[0] = -8;
 		    arr[1] = 9;
-		    arr[2] = 7;
+		    arr[2] = -7;
 		    arr[3] = 10;
-		    arr[4] = 2;
-		        
+		    arr[4] = -2;
+
 		    int[] Sortedarr = new int[5];
 		    Sortedarr[0] = -8;
 		    Sortedarr[1] = -7;
@@ -89,7 +89,7 @@ public class testSelectionSort {
 		    	assertEquals(arr[i], Sortedarr[i]);
 		    }
 	    }
-	    
+
 	@Test
 	    public void testDuplicates(){
 			int[] arr = new int[10];
@@ -103,7 +103,7 @@ public class testSelectionSort {
 		    arr[7] = 7;
 		    arr[8] = -10;
 		    arr[9] = -2;
-		        
+
 		    int[] Sortedarr = new int[10];
 		    Sortedarr[0] = -10;
 		    Sortedarr[1] = -9;
@@ -115,7 +115,7 @@ public class testSelectionSort {
 		    Sortedarr[7] = 8;
 		    Sortedarr[8] = 9;
 		    Sortedarr[9] = 10;
-	        
+
 	        /** Test data contains duplicates **/
 		    temp.basicSelectionSort(arr);
 		    for(int i = 0; i < Sortedarr.length; ++i) {
